46e19ce0c9d235d78100621d89d12c1deda47799
punkforpez <punkforpez@gmail.com>
2025-07-12 17:18:04 -0700

    Added neovim config

---
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,10 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+    opts = {
+      -- Optional: Configure Catppuccin options here, e.g., for transparency
+      transparent_background = true,
+      flavour = "mocha",
+    },
+}
